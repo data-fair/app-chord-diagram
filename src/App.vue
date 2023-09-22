58927c67452ada1c07ff72e08df647ac393886ed
SamuelMorisson <morissonsamuel@gmail.com>
2023-09-22 16:44:36 +0200

    -

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <script setup>
 import ChordDiagram from './components/ChordDiagram.vue';
 import TestChordDiagram from './components/TestChordDiagram.vue';
-import noVFor from './components/noVFor.vue';
 import FetchRequest from './components/FetchRequest.vue';
 import AxiosRequest from './components/AxiosRequest.vue';
-import testHover from './components/testHover.vue';
 </script>
 
 <template>
 
   <header>
-      <TestChordDiagram />
-      <!--ChordDiagram /-->
-      <testHover />
+      <!--TestChordDiagram /-->
+      <ChordDiagram />
       <!--AxiosRequest /-->
       <!--FetchRequest /-->
-      <!--noVFor /-->
 
 
   </header>
