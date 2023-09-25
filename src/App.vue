8a97bc99770d33d54bdbaefa05b7818ec5e0eb79
SamuelMorisson <morissonsamuel@gmail.com>
2023-09-25 15:43:50 +0200

    suppression fichiers

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup>
 import ChordDiagram from './components/ChordDiagram.vue';
-import TestChordDiagram from './components/TestChordDiagram.vue';
-import FetchRequest from './components/FetchRequest.vue';
-import AxiosRequest from './components/AxiosRequest.vue';
 </script>
 
 <template>
 
   <header>
-      <!--TestChordDiagram /-->
       <ChordDiagram />
-      <!--AxiosRequest /-->
-      <!--FetchRequest /-->
 
 
   </header>
